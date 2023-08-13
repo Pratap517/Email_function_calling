29aacfbfe46c808c2a3aaccace22cabd9c16b2ff
Prathap Reddy <65093897+Pratap517@users.noreply.github.com>
2023-08-13 05:44:23 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,38 +2,40 @@ import requests
 import json
 import gspread  # pip install gspread
 
+'''SETUP
+- google developer console: https://console.developers.google.com
+- new project -> activate drive and sheets api
+- credentials -> service account -> name + role=editor
+  ->create key and download json
+- share client_email fom json in your sheets
+'''
+
+
 gc = gspread.service_account(filename="email.json")
 sh = gc.open_by_key(
-    "1IRAW2sVEEHs-znI-oFPXbtbxYfw-xGCkEu9y3jFKbws"
+    "sheet id"
 )  # or by sheet name: gc.open("TestList")
 worksheet = sh.sheet1
 
 
-url = "http://127.0.0.1:8000/extract_info_from_email"
-
-# email = """
-# Dear Jason
-# I hope this message finds you well. I'm Shirley from Gucci;
-
-# I'm looking to purchase some company T-shirt for my team, we are a team of 2 people, and we want to get 5k t-shirt per personl
-
-# Please let me know the price and timeline you can work with;
-
-# Looking forward
-
-# Shirley Lou
-# """
+url = "http://0.0.0.0:8000/extract_info_from_email"
 
 email = """
 
-hey i'm looking for a job as a software engineer. I have 5 years of experience in datasciece
+I hope this message finds you well. I'm mike from Gucci;
+
+I'm looking to purchase some company T-shirt for my team, we are a team of 2 people, and we want to get 5k t-shirt per personl
+
+Please let me know the price and timeline you can work with;
+
+Looking forward
+
+
 """
 
-payload = {"from_email": "@example.com", "content": email}
+
+payload = {"from_email": "test@example.com", "content": email}
 # payload = {"content": email}
-
-
-# print("Payload:", payload)
 
 
 response = requests.post(url, json=payload)
@@ -45,4 +47,5 @@ response_json = response.json()
 
 # Extracted values as a list
 extracted_values = list(response_json.values())
+#writing to sheets
 worksheet.append_row(extracted_values)
